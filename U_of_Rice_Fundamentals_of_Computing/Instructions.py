af0cca842996880eb0b08220fc10d808df2e7df2
zlhTao2012 <zlinghua198@gmail.com>
2017-02-08 22:45:28 -0700

    Updated instructions

--- U_of_Rice_Fundamentals_of_Computing/Instructions.py
+++ U_of_Rice_Fundamentals_of_Computing/Instructions.py
@@ -1,4 +1,4 @@
 # Instructions:
-# 1. Go to "http://www.codeskulptor.org/", and clear pre-load python code on the code input area (left screen);
+# 1. Go to "http://www.codeskulptor.org/", and clear pre-loaded python code on the code input area (left screen);
 # 2. Copy and paste each individual python code to the code input area;
 # 3. Press the Arrow button on the top left of the code input area to run the python code;
